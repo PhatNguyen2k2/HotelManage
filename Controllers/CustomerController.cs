1295c8c498b655fca887e0bbe026fea0cbde41f8
PhatNguyen2k2 <nguyenphatbe@gmail.com>
2023-05-24 21:09:27 +0700

    add customer controller

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HotelManage.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,78 @@ namespace HotelManage.Controllers
 {
     public class CustomerController : Controller
     {
-        // GET: Customer
+        private HotelManageEntities db = new HotelManageEntities();
+        // GET: Customer/Index
         public ActionResult Index()
         {
-            return View();
+            if (HomeController.userId != "admin" && HomeController.userId != "emp")
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            else
+            {
+                ViewBag.Customers = db.Customers.ToList();
+                return View();
+            }
+        }
+        //POST: Customer/create
+        [HttpPost]
+        public ActionResult Create(string name, string email, string phone, string address)
+        {
+            try
+            {
+                ViewData["name"] = name;
+                ViewData["email"] = email;
+                ViewData["phone"] = phone;
+                ViewData["address"] = address;
+                Customer c = new Customer
+                {
+                    c_name = name,
+                    email = email,
+                    phone = phone,
+                    c_address = address,
+                };
+                db.Customers.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        //POST: Customer/update/{id}
+        public ActionResult Update(int id, string name, string email, string phone, string address)
+        {
+            ViewData["name"] = name;
+            ViewData["email"] = email;
+            ViewData["phone"] = phone;
+            ViewData["address"] = address;
+            Customer customer = db.Customers.FirstOrDefault(b => b.id == id);
+            customer.c_name = name;
+            customer.email = email;
+            customer.phone = phone;
+            customer.c_address = address;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //POST: Customer/deletecustomer/{id}
+        [HttpPost]
+        public ActionResult DeleteCustomer(int id)
+        {
+            Customer customer = db.Customers.FirstOrDefault(b => b.id == id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //POST: Customer/search
+        [HttpPost]
+        public ActionResult Search(string search)
+        {
+            ViewData["search"] = search;    
+            ViewBag.Customers = db.Customers.Where(c => c.c_name.Contains(search)).ToList();           
+            return View("Index");
         }
     }
 }
